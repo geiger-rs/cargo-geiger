aa4ef4f8eb9aafa26f0b754d44c78340dbc6a60e
Stephane Raux <stephaneyfx@gmail.com>
2020-10-16 00:31:08 -0700

    Work around cargo bug with UNC paths
    `cargo` fails to use an overriding path dependency when the manifest path given to `cargo build` is a UNC path (starting with `\\?\`).
    
    This was causing test7 test cases to fail.

--- cargo-geiger/tests/mod.rs
+++ cargo-geiger/tests/mod.rs
@@ -385,6 +385,17 @@ impl Context {
         };
         fs_extra::dir::copy(&src_path, dir.path(), &copy_options).expect("Failed to copy tests");
         let path = dir.path().canonicalize().expect("Failed to canonicalize temporary path");
+        // Canonicalizing on Windows returns a UNC path (starting with `\\?\`).
+        // `cargo build` (as of 1.47.0) fails to use an overriding path dependency if the manifest
+        // given to `cargo build` is a UNC path. Roudtripping to URL gets rid of the UNC prefix.
+        let path = if cfg!(windows) {
+            Url::from_file_path(path)
+                .expect("URL from path must succeed")
+                .to_file_path()
+                .expect("Roundtripping path to URL must succeed")
+        } else {
+            path
+        };
         let _dir = dir;
         Context { _dir, path }
     }
